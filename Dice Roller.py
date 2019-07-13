68150852dbf670cf4ae97de844b9af1a908e0405
David <dtfeldman@verion.net>
2019-07-13 11:28:24 -0400

    Rand Encount: Removed empty lines from possible choices Dice Roller: Created Dice Roller as a func

--- Dice Roller.py
+++ Dice Roller.py
@@ -1,2 +1,33 @@
+from random import randint
 
 
+def dice_roller(dice_ct=None, dice_size=None, to_beat=None):
+    if not dice_ct and not dice_size and not to_beat:
+        print("Enter number and size of dice to roll (count, size, AC to beat)")
+        raw_specs = input().split(",")
+        raw_specs = [entry.strip(" d") for entry in raw_specs]
+        dice_ct = int(raw_specs[0])
+        dice_size = int(raw_specs[1])
+        try:
+            to_beat = int(raw_specs[2])
+        except IndexError:
+            to_beat = 0
+
+    total = 0
+    dice_roll_list = []
+    total_succeed = 0
+    for i in range(0, dice_ct):
+        roll = randint(1, dice_size + 1)
+        total += roll
+        dice_roll_list.append(roll)
+        if roll >= to_beat:
+            total_succeed += 1
+
+    print("The rolls were:", *dice_roll_list)
+    print("The total was:", total)
+    if to_beat:
+        print("Total succeeding rolls:", total_succeed)
+
+
+if __name__ == '__main__':
+    dice_roller()
--- Random Encounters.py
+++ Random Encounters.py
@@ -3,8 +3,11 @@ import random
 with open("RandomEncounters.txt", 'r') as fin:
     fin_arr = []
     for line in fin:
-        fin_arr.append(line)
+        if line != "\n":
+            fin_arr.append(line)
     choice = ""
     while choice == "":
         choice = random.choice(fin_arr)
     print(choice)
+
+input()
